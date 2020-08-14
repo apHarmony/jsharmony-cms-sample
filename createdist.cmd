0b74522c73a9a6db664fc973b7ecb434bac51ca4
Andrew Palczewski <apj@apharmony.com>
2020-08-13 22:07:15 -0500

    JSHCMS-311 Remove dist from zip

--- createdist.cmd
+++ createdist.cmd
@@ -1,2 +1,2 @@
-del /q dist/release.zip
-"C:\Program Files\7-Zip\7z.exe" a -tzip dist/release.zip * -x!.git -x!dist.zip -x!node_modules -x!data -x!app.config.*.js -x!nstart.cmd -x!cert\localhost-cert.pem -x!cert -x!.gitignore -x!package-lock.json -x!createdist.cmd -x!package.json
+del /q dist\latest.zip
+"C:\Program Files\7-Zip\7z.exe" a -tzip dist/latest.zip * -x!.git -x!dist -x!node_modules -x!data -x!app.config.*.js -x!nstart.cmd -x!cert\localhost-cert.pem -x!cert -x!.gitignore -x!package-lock.json -x!createdist.cmd -x!package.json
