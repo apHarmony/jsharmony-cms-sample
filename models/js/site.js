a58e0a3368d8545f10483e01f6627b6fd89df68d
Andrew Palczewski <apj@apharmony.com>
2021-11-04 20:52:11 -0500

    Improved responsive support

--- models/js/site.js
+++ models/js/site.js
@@ -4,18 +4,7 @@
   $(document).ready(function(){
     if(!$('.xtoplinks').is(':visible')) return;
     var demoMessage = $('<div class="demo_message">* Demo Site is Read-Only *</div>');
-    demoMessage.css({
-      backgroundColor: '#00ff00',
-      position: 'absolute',
-      top: '25px',
-      right: '3px',
-      zIndex: 9,
-      padding: '1px 20px',
-      opacity: 0.75,
-      borderRadius: '15px',
-      cursor: 'default',
-    });
-    demoMessage.appendTo('body');
+    demoMessage.appendTo('.xhead');
   });
 
 })(window.{req.jshsite.instance});
